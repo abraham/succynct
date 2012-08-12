27b91eb85c60261b7d4238ce285eddd4ea13cea8
abraham <4braham@gmail.com>
2012-08-12 11:38:13 -0700

    added config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  clientId: '',
+  authorizeUrl: 'https://alpha.app.net/oauth/authenticate',
+  accessTokenUrl: 'https://alpha.app.net/oauth/access_token',
+  apiBaseUrl: 'https://alpha-api.app.net',
+  apiScope: 'stream,write_post,follow,messages',
+  apiRequestFrequency: 15 * 1000
+}
