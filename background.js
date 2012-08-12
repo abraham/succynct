4a00953aed2268b311818080e32fae6b9cc1b014
abraham <4braham@gmail.com>
2012-08-12 15:34:30 -0700

    added background with new support for hashtags and profiles in the omnibox

---
+++ background.js
@@ -0,0 +1,24 @@
+console.log('background.js');
+window.account = new window.Account();
+if (localStorage.getItem('accessToken')) {
+  window.account.set({ accessToken: localStorage.getItem('accessToken') });
+} else {
+  chrome.tabs.create({ url: chrome.extension.getURL('options.html') });
+}
+
+window.mentions = new window.Stream({ url: 'https://alpha-api.app.net/stream/0/users/me/mentions' });
+
+window.omniboxview = new window.OmniboxView();
+
+function onMessage(request, sender, sendResponse) {
+  if (request.method === 'put' && request.action === 'oauth/authenticate') {
+    window.account.set({ accessToken: localStorage.getItem('accessToken') });
+    window.account.fetch();
+  }
+  sendResponse({ });
+}
+
+chrome.extension.onMessage.addListener(onMessage);
+chrome.omnibox.setDefaultSuggestion({ description: 'Post <match>%s</match> to App.net' });
+chrome.omnibox.onInputEntered.addListener(window.omniboxview.onInputEntered);
+chrome.omnibox.onInputChanged.addListener(window.omniboxview.onInputChanged);
