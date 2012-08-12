7ed5b8f7d57741f0505894309474d9cd28757e78
abraham <4braham@gmail.com>
2012-08-12 16:41:24 -0700

    meh

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 console.log('background.js');
+window.RateLimit = [];
+window.env = 'background';
+
 window.account = new window.Account();
 if (localStorage.getItem('accessToken')) {
   window.account.set({ accessToken: localStorage.getItem('accessToken') });
@@ -14,8 +17,10 @@ function onMessage(request, sender, sendResponse) {
   if (request.method === 'put' && request.action === 'oauth/authenticate') {
     window.account.set({ accessToken: localStorage.getItem('accessToken') });
     window.account.fetch();
+    sendResponse({ });
+  } else {
+    sendResponse({ });
   }
-  sendResponse({ });
 }
 
 chrome.extension.onMessage.addListener(onMessage);
