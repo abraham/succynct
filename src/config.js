d72bb1211a408d63c7a2ae07e0dcb8af0409487c
abraham <4braham@gmail.com>
2012-08-17 21:44:20 -0700

    added alpha site base

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ var config = {
   authorizeUrl: 'https://alpha.app.net/oauth/authenticate',
   accessTokenUrl: 'https://alpha.app.net/oauth/access_token',
   apiBaseUrl: 'https://alpha-api.app.net',
+  baseUrl: 'https://alpha.app.net',
   apiScope: 'stream,write_post,follow,messages',
   apiRequestFrequency: 15 * 1000,
   apiFollowersRequestFrequency: 15 * 60 * 1000
