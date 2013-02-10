f70c89621b04a6dffed4e7b08453543d1d08eb25
abraham <4braham@gmail.com>
2013-02-09 21:43:08 -0800

    Moved analytics code to seperate file

--- src/js/background.js
+++ src/js/background.js
@@ -1,15 +1,4 @@
 console.log('background.js');
-/**
-* Enable usage tracking
-*/
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', config.get('googleAnalyticsAccount')]);
-_gaq.push(['_trackPageview']);
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
 
 /**
 * Upgrade old extensions to use chrome.storage.sync
