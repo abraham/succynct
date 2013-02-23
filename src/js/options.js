bdf14384b37247e7854b73f7a3d5fc117d258fb2
abraham <4braham@gmail.com>
2013-02-23 15:23:22 -0800

    added comment

--- src/js/options.js
+++ src/js/options.js
@@ -1,10 +1,14 @@
 console.log('options.js');
 
+/**
+ * Create an optionsview and bind it to the dom with the config and accounts
+ */
 optionsview = new OptionsView({
   el: $('#content'),
   model: config,
   collection: accounts,
 });
+
 
 /**
  * optionsview.ready will only execute once
