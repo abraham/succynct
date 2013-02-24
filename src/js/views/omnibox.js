7c703e6e1644467283a27351b1418ebfe997088a
abraham <4braham@gmail.com>
2013-02-23 16:11:06 -0800

    Moved omnibox code go views/omnibox.js

---
+++ src/js/views/omnibox.js
@@ -0,0 +1,59 @@
+console.log('views/omnibox.js')
+
+
+/**
+ * Handle interactions with the Chrome omnibox
+ */
+window.OmniboxView = Backbone.View.extend({
+
+
+  initialize: function() {
+    _.bindAll(this);
+  },
+  // events: { },
+
+
+  /**
+   * Gets called when the user hits enter in the omnibox
+   */
+  onInputEntered: function(text) {
+    if (text.indexOf('@@') === 0) {
+      chrome.tabs.create({ url: 'https://alpha.app.net/' + text.substring(2) });
+      return;
+    }
+    
+    if (text.indexOf('##') === 0) {
+      chrome.tabs.create({ url: 'https://alpha.app.net/hashtags/' + text.substring(2) });
+      return;
+    }
+    
+    if (text.indexOf('::') === 0) {
+      text = text.substring(2);
+    }
+    var post = new Post();
+    post.set({ text: text });
+    // TODO: catch errors
+    post.save();
+  },
+
+
+  /**
+   * Display suggestions to the user when the omnibox text changes
+   */
+  onInputChanged: function(text, suggest) {
+    var suggestions = [];
+    suggestions.push({ content: '::' + text, description: (256 - text.length) + ' characters remaning' });
+    
+    if (text.indexOf(' ') > -1 || text.length === 0) {
+      suggest(suggestions);
+      return;
+    }
+    if (text.indexOf('#') === 0 || text.indexOf('@') === 0) {
+      text = text.substring(1);
+    }
+    
+    suggestions.push({ content: '@@' + text, description: 'View the @<match>' + text + "</match> profile on App.net" });
+    suggestions.push({ content: '##' + text, description: 'Search the #<match>' + text + "</match> hashtag on App.net" });
+    suggest(suggestions);
+  }
+});
