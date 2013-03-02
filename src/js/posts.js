5aa9382fa753777d2cbeb21a9f0f8c1dcd2fd02b
abraham <4braham@gmail.com>
2013-03-02 12:26:21 -0800

    Setting up for a generic posts collection to extend for everything

--- src/js/posts.js
+++ src/js/posts.js
@@ -72,19 +72,25 @@ window.Post = Backbone.Model.extend({
 
 
 /**
- * This is left over from a previous design choice. It may be going away.
+ * Generic collection for handling posts
  */
-var Polling = Backbone.Collection.extend({
+var Posts = Backbone.Collection.extend({
+
+
   initialize: function(options) {
     _.bindAll(this, 'error');
     _.extend(this, options);
   },
+
+
   // update: function() {
   //   if (window.account && window.account.get('accessToken') && config.get(this.configName)) {
   //     this.fetch({ error: this.error });
   //   }
   //   return this;
   // },
+
+
   error: function(collection, response, options) {
     // TODO: update copy of notifications
     if (response.status === 401) {
@@ -112,76 +118,6 @@ var Polling = Backbone.Collection.extend({
     }
     return this;
   }
+
+
 });
-
-
-// /**
-//  * A collection of interactions
-//  */
-// var Interactions = Polling.extend({
-//   model: Interaction,
-//   url: 'https://alpha-api.app.net/stream/0/users/me/interactions',
-//   initialize: function() {
-//     _.bindAll(this, 'checkForNew');
-//   },
-//   checkForNew: function() {
-//     if (accounts.length === 0) {
-//       return false;
-//     }
-//     this.fetch({
-//       error: this.error,
-//       update: true,
-//       data: {
-//         count: 2 // TODO: start using since_id
-//       },
-//       headers: {
-//         'Authorization': 'Bearer ' + accounts.at(0).get('access_token'),
-//         // HACK: should be applied globally
-//         'X-ADN-Migration-Overrides': 'response_envelope=1&disable_min_max_id=1&follow_pagination=1&pagination_ids=1'
-//       }
-//     });
-//   },
-//   parse: function(response) {
-//     return response.data;
-//   },
-//   // renderNotification: function(model, index, array) {
-//   //   if (this.notificationType === 'mentions') {
-//   //     this.renderMentionNotification(model);
-//   //   } else {
-//   //     console.log('this.notificationType', this.notificationType);
-//   //   }
-//   // },
-//   // renderMentionNotification: function(model) {
-//   //   var notification = new TextNotificationView({
-//   //     image: model.get('user').avatar_image.url,
-//   //     title: 'Mentioned by @' + model.get('user').username + ' on ADN',
-//   //     body: model.get('text'),
-//   //     url: 'https://alpha.app.net/' + model.get('user').username + '/post/' + model.get('id'),
-//   //     type: 'Mention'
-//   //   });
-//   //   notification.render();
-//   // },
-//   // filterNewPosts: function() {
-//   //   var models = [];
-//   //   var lastCreatedAt = localStorage.getItem(this.configName + '_lastCreatedAt');
-//   //   // Don't notify for new polling channels
-//   //   if (!lastCreatedAt) {
-//   //     localStorage.setItem(this.configName + '_lastCreatedAt', (new Date()).getTime());
-//   //     return this;
-//   //   }
-//   //   
-//   //   // Reject posts by authenticated account
-//   //   models = this.reject(function(post){ return post.get('user').id === account.get('id'); });
-//   //   // Filter out old posts
-//   //   models = models.filter(function(post){ return (new Date(post.get('created_at'))).getTime() > parseInt(lastCreatedAt); });
-//   //   
-//   //   // Store latest created_at date for future matches
-//   //   if (_.first(models)) {
-//   //     localStorage.setItem(this.configName + '_lastCreatedAt', (new Date(_.first(models).get('created_at'))).getTime());
-//   //   }
-//   //   _.each(models, this.renderNotification);
-//   // }
-// });
-// 
-// 
-// window.interactions = new Interactions();
