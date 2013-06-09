07a9c314390e34ba2e5ebc955ca60cc907febf7c
abraham <4braham@gmail.com>
2013-06-08 17:27:47 -0700

    Added options link to the accounts page on alpha

---
+++ src/js/content_alpha.js
@@ -0,0 +1,6 @@
+var item = '<ul class="nav nav-list"> \
+  <li class="nav-header">Succyct Extension</li><li class=""> \
+    <a href="chrome-extension://hngjfhijhmcechdkbgopbgghajokogdi/options.html" data-non-pjax="1">Options</a> \
+  </li> \
+</ul>';
+$('.yui3-u-1-4.m-yui3-u-1').append(item);
